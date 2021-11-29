eabb514fe25d74341874a82ee05fd3202eff7d3f
joffrey <joffreycaugnon@gmail.com>
2021-11-29 18:03:03 +0100

    rename URLs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'items#index' 
-  resources :items, only: [:index, :show]
-  resources :carts, only: [:create, :update, :destroy, :show], path: 'mon_panier'
-  resources :orders, only: [:create, :show]
-  devise_for :users, path: 'mon_profil'
+  resources :items,:path => "/NFT_cat", only: [:index, :show]
+  resources :carts,:path => "/cart", only: [:create, :update, :destroy, :show]
+  resources :orders,:path => "/order_history",  only: [:create, :show]
+  devise_for :users,:path => "/profile"
 end
