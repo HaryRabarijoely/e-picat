80d34cfe1f8adb3cf4ceff76343b33ef865b4a81
KevinPiriou <kevinpiriou@yahoo.fr>
2021-11-30 21:52:44 +0100

    not working

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -2,20 +2,23 @@ class CheckoutController < ApplicationController
   def create
     @user = current_user
     @total = params[:total].to_d
+    
     @session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
       line_items: [
         {
           name: 'Rails Stripe Checkout',
-          amount: (@total*100),
+          amount: (@total).to_i,
           currency: 'eur',
           quantity: 1
         },
       ],
       success_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}',
       cancel_url: checkout_cancel_url
-    ) 
+    )
+    
     respond_to do |format|
+      
       format.js
     end
   end
