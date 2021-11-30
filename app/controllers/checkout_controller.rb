7e4ad3a23a8934abb3f31c07b06fd9f89f952506
KevinPiriou <kevinpiriou@yahoo.fr>
2021-11-30 18:34:56 +0100

    add stripe

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,32 @@
+class CheckoutController < ApplicationController
+  def create
+    @user = current_user
+    @total = params[:total].to_d
+    @session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          name: 'Rails Stripe Checkout',
+          amount: (@total*100),
+          currency: 'eur',
+          quantity: 1
+        },
+      ],
+      success_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}',
+      cancel_url: checkout_cancel_url
+    ) 
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def success
+    @session = Stripe::Checkout::Session.retrieve(params[:session_id])
+    @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
+  end
+
+  def cancel
+    @session = Stripe::Checkout::Session.retrieve(params[:session_id])
+    @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
   resources :carts,:path => "/cart", only: [:create, :update, :destroy, :show]
   resources :order,:path => "/order_history",  only: [:create, :index]
   devise_for :users,:path => "/profile"
+
+  scope '/checkout' do
+    post 'create', to: 'checkout#create', as: 'checkout_create'
+    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+    get 'success', to: 'checkout#success', as: 'checkout_success'
+  end
+  
 end
