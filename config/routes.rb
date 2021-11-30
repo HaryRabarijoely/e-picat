9d91ff19375724d37427fdfe91b3b71bfc673cb8
KevinPiriou <kevinpiriou@yahoo.fr>
2021-11-30 23:19:16 +0100

    stripe_legacy

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :items,:path => "/NFT_cat", only: [:index, :show]
   resources :carts,:path => "/cart", only: [:create, :update, :destroy, :show]
   resources :order,:path => "/order_history",  only: [:create, :index]
+  resources :charges
   devise_for :users,:path => "/profile"
 end
