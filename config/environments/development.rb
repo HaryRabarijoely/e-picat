5155ef490640960bd3f515ad41840982d5d472c3
KevinPiriou <kevinpiriou@yahoo.fr>
2021-11-30 15:44:42 +0100

    change in true

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # Config Mailer
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
  # config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
