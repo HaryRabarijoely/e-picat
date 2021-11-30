a0fee2f1474f72662ee918c246328fa2baf0c1db
KevinPiriou <kevinpiriou@yahoo.fr>
2021-11-30 14:27:20 +0100

    config_mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   before_create :user_cart_creation
-  
+  after_create :welcome_send
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -16,5 +16,9 @@ class User < ApplicationRecord
   # Assign a new cart for each new user
   def user_cart_creation
     self.cart = Cart.create
+  end
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
   end
 end
