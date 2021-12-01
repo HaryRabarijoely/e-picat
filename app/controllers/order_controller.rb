a7eecac42d7fc975b7dc7a7e091f7367777c2809
joffrey <joffreycaugnon@gmail.com>
2021-12-01 18:23:13 +0100

    begin order controller

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,9 +1,33 @@
 class OrderController < ApplicationController
-  def create
-    @order = Order.new
-  end
-
   def index
     @orders = Order.all
   end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    cart = current_user.cart
+    @order = Order.new(
+      user_id: current_user.id,
+      customer_stripe_id: "je sais pas",
+      price: (@total*100).to_i,
+    )
+    puts "*" * 80
+    puts @order
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
 end
