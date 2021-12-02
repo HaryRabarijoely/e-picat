11df917beb11868e85d7f6ba5353b28c5ae4ce61
fleopaulD <flo_thp@netc.pl>
2021-12-02 14:55:40 +0100

    add flashs messages for carts and orders confirmations

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -7,18 +7,28 @@ class CartsController < ApplicationController
     cart = Cart.find_by(user: current_user)
     item = Item.find(params[:id])
     cart.items << item
-    cart.save
-    cart.price_update
-    redirect_to cart_path
+    if cart.save
+      cart.price_update
+      flash[:success] = "Le produit a bien été ajouté à votre panier"
+      redirect_to cart_path
+    else
+      flash[:warning] = "Il y a eu un problème lors de l'ajout du produit au panier"
+      redirect_to root_path
+    end
   end
 
   # Remove an element in the current_user's cart
   def destroy
     cart = Cart.find_by(user: current_user)
     item = Item.find(params[:id])
-    cart.items.delete(item)
-    cart.price_update
-    redirect_to cart_path
+    if cart.items.delete(item)
+      cart.price_update
+      flash[:success] = "Le produit a bien été supprimé de votre panier"
+      redirect_to cart_path
+    else
+      flash[:warning] = "Il y a eu un problème lors de la suppression du produit"
+      redirect_to root_path
+    end
   end
 
   def show
